31c32cb10cc661a0180a4173796982c1a43927a0
Justin Restivo <justinjrestivo@gmail.com>
2017-11-02 15:09:10 -0500

    Create s3deletebuckets_standalone.sh

---
+++ s3deletebuckets_standalone.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/bash
+
+#list all vaults in Cleversafe Dev
+#delete all s3 test framework vaults ("s3-" or $prefix)
+#need to make testing prefix more complex/random
+#this is intentionally iterative to speed up the process
+
+#needs .s3cfg, an admin name, and a prefix
+#for your customizations
+export admin=justin_restivo
+export prefix=s3-
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+echo -en "\n${RED}This script will attempt to purge any vaults with the specified prefix: $prefix ${NC}\n"
+
+#variables
+export vaultlist_=/tmp/s3dir/s3vaultlist_
+export vaultlist=/tmp/s3dir/s3vaultlist
+export logfile=/tmp/s3dir/s3deletelog
+export itemlist_=/tmp/s3dir/s3itemlist_
+export itemlist=/tmp/s3dir/s3itemlist
+export s3dir=/tmp/s3dir/
+
+#verify configuration
+#requires configured s3cmd
+if [ -e ~/.s3cfg ]
+        then echo -en "\ns3cfg found, continuing..\n"
+        else
+                echo -en "\ns3cfg not found..\n"
+                exit 0
+fi
+
+if [ -d $s3dir ]
+        then
+                rm -rf {$s3dir/*}
+        else
+                mkdir -p $s3dir
+fi
+
+function gatherlist {
+s3cmd ls > $vaultlist_
+#delete the unnessecay prefix to give us an easy to read vault list
+#removes the first 18 characters from every line
+#remove any lines that do not have our test suite prefix: "s3-" or $prefix
+sed -ri 's .{18}  ' $vaultlist_
+cat $vaultlist_ | grep $prefix &> $vaultlist
+#sed -ri '/$prefix/!d' $vaultlist
+}
+gatherlist && echo -en '\nLsit of vaults:\n' && cat $vaultlist
+
+#make sure the logfile is clear
+#not used right now
+#echo > $logfile
+#&>>$logfile
+
+#first pass
+#delete all vaults that do not have 1) items inside. 2) versions inside. 3) ACLs
+echo -en '\nDeleting vaults\n'
+function deletevaults {
+        cat $vaultlist | while read line ; do s3cmd rb --recursive --force $line ; done
+}
+deletevaults & deletevaults & deletevaults
+
+function itemlistgen {
+s3cmd la > $itemlist_
+#clean up itemlist
+#delete the unnessecay prefix to give us an easy to read vault list
+#removes the first 18 characters from every line
+#remove any lines that do not have our test suite prefix: "s3-" or $prefix
+sed -ri '/^\s*$/d' $itemlist_
+cat $itemlist_ | grep $prefix &> $itemlist
+#sed -ri '/$prefix/!d' $itemlist
+sed -ri 's .{29}  ' $itemlist
+}
+timeout --signal=SIGINT 30 itemlistgen
+
+echo -en '\nLsit of vaults containing keys:\n'
+
+echo -en '\nDeleting items\n'
+function deleteitems {
+#second pass
+#delete vaults with items inside
+#clean up vaults with items inside
+cat $itemlist | while read line ; do s3cmd del --recursive --force $line ; done
+}
+deleteitems
+deletevaults
+
+echo -en '\nRunning vaultfix\n'
+function vaultfix {
+#third pass
+#move broken keys to new vault for deletion
+#attempt a vaultfix
+cat $itemlist | while read line ; do s3cmd fixbucket --recursive --force $line ; done
+}
+vaultfix
+
+echo -en '\nMoving empty objects\n'
+function emptyfiles {
+#make a temp vault to move empty files
+s3cmd mb s3://s3delete
+cat $itemlist | while read line ; do /s3cmd mv $line s3://s3delete --recursive --force ; done
+#remove the temp vault
+s3cmd rb s3://s3delete --recursive --force
+}
+emptyfiles
+
+echo -en '\nSetting ACLs\n'
+function acls {
+#fourth pass
+#add access to delete vaults with ACLs
+#should be a superadmin account, not justin_Restivo
+cat $vaultlist | while read line ; do s3cmd setacl --recursive --force --acl-grant=full_control:$admin $line ; done
+cat $itemlist | while read line ; do s3cmd setacl --recursive --force --acl-grant=full_control:$admin $line ; done
+}
+acls
+
+#delete all vaults again
+echo -en '\nDeleting vaults\n'
+deletevaults & deletevaults & deletevaults
+
+###########
+#Agressive#
+###########
+
+echo -en '\nDeleting Vaults\n'
+function deletecors {
+#fifth pass
+#add access to delete vaults CORS
+        cat $vaultlist | while read line ; do s3cmd delcors --recursive --force $line ; done
+}
+deletecors
+
+echo -en '\nDeleting Vault Policies\n'
+function deletepolicy {
+#sixth pass
+#remove vault policies
+        cat $vaultlist | while read line ; do s3cmd delpolicy --recursive --force $line ; done
+}
+deletepolicy
+
+echo -en '\nDeleting Vault Lifecycles\n'
+function deletelifecycle {
+#seventh pass
+#remove vault lifecycles
+        cat $vaultlist | while read line ; do s3cmd dellifecycle --recursive --force $line ; done
+}
+deletelifecycle
+
+echo -en '\nExpiring Vaults\n'
+function expirevaults {
+#eighth pass
+#expire all vaults specified
+        cat $vaultlist | while read line ; do s3cmd expire --recursive --force --expiry-days=1 $line ; done
+}
+expirevaults
+
+#delete all vaults again
+#Last Pass
+echo -en '\nDeleting vaults\n'
+deletevaults & deletevaults & deletevaults
+
+gatherlist && echo -en '\nLsit of vaults:\n' && cat $vaultlist && echo -en '\nDone\n\n'
+
+exit 0
